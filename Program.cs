198c517641fb010388ca758b70ac9188319681e1
csenger7 <lengyelcs@kkszki.hu>
2024-10-18 10:24:25 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem, adja meg a diák pontszámát (0-100): ");
+            int pontszam = int.Parse(Console.ReadLine());
+
+            if (pontszam < 0 || pontszam > 100)
+            {
+                Console.WriteLine("Érvénytelen pontszám");
+            }
+            else if (pontszam < 50)
+            {
+                Console.WriteLine("Elégtelen");
+            }
+            else if (pontszam < 60)
+            {
+                Console.WriteLine("Elégséges");
+            }
+            else if (pontszam < 80)
+            {
+                Console.WriteLine("Közepes");
+            }
+            else if (pontszam < 90)
+            {
+                Console.WriteLine("Jó");
+            }
+            else
+            {
+                Console.WriteLine("Jeles");
+            }
+        }
+    }
+}
